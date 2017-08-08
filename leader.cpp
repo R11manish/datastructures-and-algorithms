18f197dca9884c9f72756a52e7fde4e313beb28d
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-08-08 22:38:07 +0530

    Added leader.cpp

---
+++ leader.cpp
@@ -0,0 +1,35 @@
+/*
+	Finding the leader in a list of values. 
+	A value is a leader, if it is greater 
+	than all its successive values
+	17, 29, 2, 38, 57, 19, 43, 21, 3, 7 --> 43
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int size, i;
+	printf("Enter the size of the list: ");
+	scanf("%d", &size);
+	int arr[size];
+	for(i=0; i<size; i++)
+		scanf("%d", &arr[i]);
+	int j, k;
+	for(j=0; j<size-1; j++)
+	{
+		int flag=1;
+		for(k=j+1; k<size; k++)
+		{
+			if(arr[k]>arr[j])
+			{
+				flag=0;
+				break;
+			}
+		}
+		if(flag==1)
+			printf("\n%d is a LEADER!", arr[j]);
+	}
+	printf("\n");
+	return 0;
+}
